459ff6bbb8fdadd649aedbfba25a4e7034717e99
Firas Belhadj <firasakahawm@gmail.com>
2023-09-28 19:35:36 +0100

    test with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies-app';
+  today = new Date();
+  imageUrl = 'https://cdn.pixabay.com/photo/2013/11/28/10/02/camera-219958_1280.jpg';
+  imageWidth = 100;
+  price = 10;
+  sellingPrice = 15.50;
+  attributeValue= "movies app test value";
+  timeNow = new Date;
+  pcolor = "";
+  setTimeToNow(){
+    console.log(new Date)
+    this.timeNow=new Date;
+  }
+  setTextColor(_color: string){
+    this.pcolor= _color;
+
+
+  }
 }
